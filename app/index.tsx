9e971ea7d756bd971416dd58605230dc7240a430
YussefMo <youssefapoulila@gmail.com>
2025-07-10 20:27:52 +0300

    initial commit

--- app/index.tsx
+++ app/index.tsx
@@ -3,7 +3,7 @@ import { Text, View } from "react-native";
 export default function App() {
   return (
     <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-5xl font-bold text-center text-primary font-quicksand-light">
+      <Text className="text-5xl text-center text-primary font-quicksand-bold">
         Welcome to Nativewind!
       </Text>
     </View>
